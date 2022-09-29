7cde1a1fe509995daa2e6470c2e957cf5ea0d306
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-09-29 16:21:28 -0400

    Added image to header
    added header image

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div>
+          <img  style={{ width: 150, height: 100 }} src={process.env.PUBLIC_URL + "/mockbuster.jpeg"} alt="mockbuster" />
+        </div>
+    )
+}
+
+export default Header
