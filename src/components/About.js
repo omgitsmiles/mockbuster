562292b405dd0d9ffc28989b6d9103cab5133432
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-01 19:31:34 -0400

    patch
    added a patch req for the about me section

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,24 @@ function About({ movies, setMovies }) {
     function handleNewDescription(e) {
         e.preventDefault()
         const searched = movies.find(movie => captureTitle.toLowerCase() === movie.title.toLowerCase())
-        console.log(searched.id)
+        fetch(`http://localhost:3004/movies/${searched.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({description: description})
+        })
+        .then(r => r.json())
+        .then(updatedDescription => movies.map(movie => {
+            const updated =  movies.map(movie => movie.id === searched.id ? updatedDescription : movie)
+            setMovies(updated)
+        }))
     }
 
     return (
         <div>
             <h1>About Us</h1>
-            <p>We here at Mockbuster believe nostalgia is the lifeforce of our consumer habbits. 
+            <p>We here at Mockbuster believe nostalgia is the lifeforce of our consumer habits. 
             So to take full advantage we've rebranded what looks familiar to sell old movies.</p>
             <p>Since the last time we actually since these movies was 30(?!) years ago, if we got the description wrong, help us fix it!</p>
         <form onSubmit={handleNewDescription}>
