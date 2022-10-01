fa0e8923201bb6c7600b3daa6a0b827344123a70
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-01 18:33:04 -0400

    about form
    added a form to about to maybe patch?

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
 
 function About() {
     return (
         <div>
-            ABOUT
+            <h1>About Us</h1>
+            <p>We here at Mockbuster believe nostalgia is the lifeforce of our consumer habbits. 
+            So to take full advantage we've rebranded what looks familiar to sell old movies.</p>
+            <p>Since we actually since these movies in 30(?!) years, if we got the description wrong, help us fix it!</p>
+        <form>
+   
+                <TextField
+                className="addMovie"
+                required
+                id="filled-basic"
+                label="Title"
+                variant="filled"
+                />
+        </form>
         </div>
     )
 }
